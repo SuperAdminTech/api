894484992908631f0b17912d131f91b85786976a
Lluis Santos <lluis@ngynx.com>
2020-08-02 15:47:16 +0200

    fix(docker): fixed docker entrypoint

--- docker/prod/api/docker-entrypoint.sh
+++ docker/prod/api/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-envsubst < docker/prod/api/vhost.conf > /etc/apache2/sites-available/000-default.conf
-
-chmod +x public src config
+composer run-script post-update-cmd
+chmod +x src config public
+chown -R www-data:www-data var/cache var/log
 
 apache2ctl -DFOREGROUND
