76657172c00f4ae790ff15202b9406c5a8d01ac7
Lluis Santos <lluis@ngynx.com>
2020-08-02 16:09:34 +0200

    fix(docker): fixed docker entrypoint

--- docker/prod/api/docker-entrypoint.sh
+++ docker/prod/api/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+env | grep -v "^_" >> /etc/apache2/envvars
+
 composer run-script post-update-cmd
 chmod +x src config public
 chown -R www-data:www-data var/cache var/log
