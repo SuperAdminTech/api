88ee0c36b0828d24930b0acc633b69c98fd3b6f5
Lluis Santos <lluis@ngynx.com>
2020-08-03 16:50:31 +0200

    fix(deps): removed duplicate dependencies and implemented more tests

--- tests/Security/SignInTest.php
+++ tests/Security/SignInTest.php
@@ -13,25 +13,31 @@ class SignInTest extends WebTestCase
     use RecreateDatabaseTrait;
     use ApiUtilsTrait;
 
-    public function testSignInOkDefault(): void {
+    public function testSignInOkDefaultShouldSuccess(): void {
         $credentials = ['username' => 'test@example.com', 'password' => 'secret', 'realm' => 'default'];
         $this->json()->request('POST', '/app/token', $credentials);
         $this->assertResponseIsSuccessful();
     }
 
-    public function testSignInOkApp0(): void {
+    public function testSignInOkDefaultShouldReturnToken(): void {
+        $credentials = ['username' => 'test@example.com', 'password' => 'secret', 'realm' => 'default'];
+        $resp = $this->json()->request('POST', '/app/token', $credentials);
+        $this->assertObjectHasAttribute('token', json_decode($resp->getContent()));
+    }
+
+    public function testSignInOkApp0ShouldSuccess(): void {
         $credentials = ['username' => 'test@example.com', 'password' => 'secret0', 'realm' => 'app0'];
         $this->json()->request('POST', '/app/token', $credentials);
         $this->assertResponseIsSuccessful();
     }
 
-    public function testSignInBadCredentials(): void {
+    public function testSignInBadCredentialsShouldFail(): void {
         $credentials = ['username' => 'test@example.com', 'password' => 'badpass', 'realm' => 'default'];
         $this->json()->request('POST', '/app/token', $credentials);
         $this->assertResponseStatusCodeSame(401);
     }
 
-    public function testSignInBadRealm(): void {
+    public function testSignInBadRealmShouldFail(): void {
         $credentials = ['username' => 'test@example.com', 'password' => 'secret', 'realm' => 'app0'];
         $this->json()->request('POST', '/app/token', $credentials);
         $this->assertResponseStatusCodeSame(401);
