96966fa78572c4e2345a1e50356663c9a393f46b
Lluis Santos <lluis@ngynx.com>
2020-08-26 18:02:51 +0200

    fix(register): bugfix in register

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -19,7 +19,7 @@ use App\Dto\SignUp;
  *     collectionOperations={
  *          "get"={"security"="is_granted('ROLE_ADMIN')"},
  *          "post"={
- *              "path"="/users/sign_up",
+ *              "path"="/public/users/sign_up",
  *              "input"=SignUp::class,
  *              "openapi_context"={
  *                  "summary"="Call to register users",
