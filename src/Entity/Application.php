1b51a02dc40e723c8c7eca45f546575eff221e3c
Lluis Santos <lluis@ngynx.com>
2020-08-26 18:13:18 +0200

    fix(application): fixes #14

--- src/Entity/Application.php
+++ src/Entity/Application.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\MaxDepth;
 
 /**
@@ -25,18 +26,21 @@ class Application extends Base {
     /**
      * @var string $name
      * @ORM\Column(type="string")
+     * @Groups({"public:read", "super:write"})
      */
     public $name;
 
     /**
      * @var string $realm
      * @ORM\Column(type="string")
+     * @Groups({"super:read", "super:write"})
      */
     public $realm;
 
     /**
      * @var User
      * @ORM\OneToMany(targetEntity=User::class, mappedBy="application")
+     * @Groups({"super:read", "super:write"})
      * @MaxDepth(1)
      */
     public $users;
