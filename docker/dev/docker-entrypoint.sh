9d2d8b168c8e5b14853489af51cdf8f015fb60c2
Lluis Santos <lluis@ngynx.com>
2022-04-27 06:59:28 +0200

    fix(ci): integrated codecov

--- docker/dev/docker-entrypoint.sh
+++ docker/dev/docker-entrypoint.sh
@@ -6,8 +6,13 @@ export APP_VERSION
 
 case $1 in
   dev)
-    composer install --no-interaction
     symfony server:start --no-tls --allow-http --port=8000
+    ;;
+  test)
+    vendor/bin/phpunit
+    ;;
+  coverage)
+    XDEBUG_MODE=coverage vendor/bin/phpunit -d memory_limit=1G --coverage-clover coverage.xml --do-not-cache-result --process-isolation
     ;;
   *)
     exec "$@"
